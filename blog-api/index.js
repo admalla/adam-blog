fe896ad77a413d6198b0c7dd8f91ca8a3fbd716b
admalla <maasovec@gmail.com>
2022-01-27 22:24:34 +0300

    указал путь к public

--- blog-api/index.js
+++ blog-api/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('../public'));
 app.use(
   fileUpload({
     createParentPath: true,
