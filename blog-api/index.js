03bd84eabe31c91e241eb997062431ef81522ec1
admalla <maasovec@gmail.com>
2022-01-27 22:47:26 +0300

    подправил путь

--- blog-api/index.js
+++ blog-api/index.js
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '../public'));
 
 initRoutes(app);
 
