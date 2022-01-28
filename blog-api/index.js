312be48a9117d3b53b862700a1324ed0bdf8be74
admalla <maasovec@gmail.com>
2022-01-28 16:21:13 +0300

    правки

--- blog-api/index.js
+++ blog-api/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('../public'));
+app.use(express.static('client/build'));
 app.use(
   fileUpload({
     createParentPath: true,
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname + '../public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 initRoutes(app);
 
