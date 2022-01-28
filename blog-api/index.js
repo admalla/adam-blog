c579548a124762528f4b1081ef80629928acdaf6
admalla <maasovec@gmail.com>
2022-01-28 18:40:52 +0300

    правки

--- blog-api/index.js
+++ blog-api/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 initRoutes(app);
